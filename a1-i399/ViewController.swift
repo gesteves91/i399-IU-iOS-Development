bc42c62082be6c7ac0c306cf7a5c20175473769f
Luke D. Street <ldstreet@149-160-229-80.dhcp-bl.indiana.edu>
2014-10-26 18:18:26 -0400

    Initial Commit

---
+++ a1-i399/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  a1-i399
+//
+//  Created by Luke D. Street on 10/26/14.
+//  Copyright (c) 2014 Luke D. Street. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
